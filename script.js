c20db8676689b5427bc2c5ab0fb2c2cc8ac08c3a
Picinelli <cinelli.dev@gmail.com>
2022-02-07 01:18:09 -0300

    Versão 0.6 - Alerta ganhou em X jogadas & prompt pergunta iniciar novamente

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let mesa = document.querySelector('.mesa');
 let perguntaQuantidade = null;
 let imagens = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
 let imagensPares = [];
+let contadorFim = 0
+let contadorJogadas = 0
 
 
 // Pergunta ao jogador a quantidade de cartas que quer jogar, limitado de 4 a 14
@@ -26,18 +28,19 @@ function criarCartas () {
     let contador = 0
     while (contador < (perguntaQuantidade)) {
     mesa.innerHTML = mesa.innerHTML + `
-    <div class="card" onclick="virarCartas(this)">
-        <div class="front-face face">
+    <div class="card" onclick="virarCartas(this)" data-identifier="card">
+        <div class="front-face face" data-identifier="back-face">
             <img src="conteudo/papagaio-padrao.svg" alt="Papagaio-Padrao">
         </div>
-        <div class="back-face face">
-        <img src="conteudo/${imagensPares[contador]}" alt="Papagaio-Verso">
+        <div class="back-face face" data-identifier="front-face">
+            <img src="conteudo/${imagensPares[contador]}" alt="Papagaio-Verso">
         </div>
     </div>
     `
     contador++
     }
 }
+// funcao dada pelo exercicio para embaralhar
 function comparador() { 
 	return Math.random() - 0.5; 
 }
@@ -51,47 +54,69 @@ function misturaCarta() {
     }
     imagensPares.sort(comparador)
 }
-/*function virarCartas(elemento) {
-    let frente = elemento.querySelector('.front-face')
-    let tras = elemento.querySelector('.back-face');
-    frente.classList.add('virar-frente')
-    tras.classList.add('virar-tras');
-}
-*/
+// Define as ações das cartas no clique
 function virarCartas(elemento) {
+    contadorJogadas++
+
     const cartaVirada = document.querySelector('.card .virar-frente');
-    //  Checa se já tem uma carta virada, e caso haja, 
-    //  se ela possui o mesmo conteudo da que acabou de ser virada
     if (cartaVirada !== null) {
         const carta1 = cartaVirada.parentNode.querySelector('.back-face').innerHTML;
         const carta2 = elemento.querySelector('.back-face').innerHTML;
+// se as cartas forem iguais, manter virada.
         if (carta1 === carta2) {
             manterCartasViradas(elemento);
             manterCartasViradas(cartaVirada.parentNode);
+            setTimeout(() => checarFim(), 1000)
+// se as cartas forem diferentes, aguardar 1s e desvirar
         } else if (carta1 !== carta2) {
             virarCarta(elemento);
             setTimeout(() => desvirarCartas(elemento), 1000)
             setTimeout(() => desvirarCartas(cartaVirada.parentNode), 1000);
         }
+// se nenhum dos casos anteriores acontecerem, será a primeira carta clicada
     } else {
         virarCarta(elemento);
     }
 }
-
+// funcao que mantem as cartas viradas, caso forem do mesmo tipo
 function manterCartasViradas(carta) {
     carta.querySelector('.front-face').classList.remove('virar-frente');
     carta.querySelector('.back-face').classList.remove('virar-tras');
     carta.querySelector('.front-face').classList.add('virar-frente-fixo');
     carta.querySelector('.back-face').classList.add('virar-tras-fixo');
 }
-
+// funcao que vira a carta
 function virarCarta(elemento) {
     elemento.querySelector('.front-face').classList.add('virar-frente');
     elemento.querySelector('.back-face').classList.add('virar-tras');
 }
+// funcao que desvira a carta
 function desvirarCartas(carta) {
     carta.querySelector('.front-face').classList.remove('virar-frente');
     carta.querySelector('.back-face').classList.remove('virar-tras');
+}
+function checarFim() {
+    contadorFim++
+    if (contadorFim === (perguntaQuantidade / 2)) {
+        window.alert(`Parabéns! Você ganhou em ${contadorJogadas} jogadas!`)
+        perguntaIniciarNovamente()
+    }
+}
+function perguntaIniciarNovamente () {
+    let pergunta = window.prompt('Gostaria de jogar novamente? (s ou n)')
+    if (pergunta === 'n') {
+        window.alert('Tudo certo! Obrigado por jogar :)')
+    } else if (pergunta === 's') {
+        // declarando as variaveis novamente para resetar o jogo e chamando a funcao 
+        // "perguntaCartas" para reiniciar todo o processo
+        mesa.innerHTML = '';
+        perguntaQuantidade = null
+        imagensPares = [];
+        contadorFim = 0
+        contadorJogadas = 0
+        imagens = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
+        perguntaCartas();
+    }
 }
 
 
